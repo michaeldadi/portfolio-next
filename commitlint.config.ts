18bce8a42714a4dd20a3f2810eff1216320eede5
Michael Dadi <13662053+michaeldadi@users.noreply.github.com>
2025-08-13 16:03:15 -0400

    feat: initial commit

---
+++ commitlint.config.ts
@@ -0,0 +1,35 @@
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+  },
+};
+
+export default config;
---
+++ prettier.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+};
+
+export default config;
